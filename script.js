99d9eff0c3f491a7a7080868e8d706187ac3d0d8
Kevin Lewis <phazonoverload@users.noreply.github.com>
2017-04-12 11:51:05 +0100

    Added box-sizing to the banner

--- script.js
+++ script.js
@@ -26,4 +26,4 @@ for(var i=0; i<hacksmithsData.length; i++) {
   hacksmithsCont.appendChild(a);
 }
 document.body.prepend(hacksmithsCont);
-document.write("<style>body{padding-top:2em;}#hacksmiths-banner{position:absolute;top:0;left:0;width:100%;background:#37474F;padding:0.5rem;}#hacksmiths-banner a{color:white !important; font-family:'Open Sans',sans-serif;text-decoration:none !important;margin-right:1em !important; border:none !important;}#hacksmiths-banner a:first-child{font-weight:bold;}@media screen and (max-width:800px){body{padding-top:0;} #hacksmiths-banner{display:none;}}</style>");
+document.write("<style>body{padding-top:2em;}#hacksmiths-banner{box-sizing:border-box;position:absolute;top:0;left:0;width:100%;background:#37474F;padding:0.5rem;}#hacksmiths-banner a{color:white !important; font-family:'Open Sans',sans-serif;text-decoration:none !important;margin-right:1em !important; border:none !important;}#hacksmiths-banner a:first-child{font-weight:bold;}@media screen and (max-width:800px){body{padding-top:0;} #hacksmiths-banner{display:none;}}</style>");
